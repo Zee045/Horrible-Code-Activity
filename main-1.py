cc131de94b538941b84ac6595005b50f2c26b94f
Zee045 <167368409+Zee045@users.noreply.github.com>
2024-09-24 12:32:57 -0400

    Update and rename main.py to main-1.py

---
+++ main-1.py
@@ -0,0 +1,13 @@
+print("Simple Calculator program")
+def add(a, b):
+  return a + b #Add two values 
+
+def subtract(a, b):
+  return a -b #subtract two values
+
+def multiply(a, b):
+  return a * b #multiple two values
+
+def main
+
+
