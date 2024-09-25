ab48cad249318c833f0ae19c157acef03399fa59
Zee045 <167368409+Zee045@users.noreply.github.com>
2024-09-24 21:11:52 -0400

    Update main-1.py

--- main-1.py
+++ main-1.py
@@ -8,12 +8,16 @@ def subtract(a, b):
 def multiply(a, b):
   return a * b #multiple two values
 
+def divide(a, b):
+  return a / b
+
 def main():
   Print("Select operation:")
   print("1- Add")
   print("2- Subtract")
   print("3- Multiply")
-  choice = input("Enter choice of 1/2/3:")
+  print("4- divide")
+  choice = input("Enter choice of 1/2/3/4:")
   if choice in ['1','2','3']:
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
@@ -23,6 +27,8 @@ def main():
       print(f"The subtraction result is: {subtract(num1, num2)}")
     elif choice == '3':
       print(f"The multiplication result is: {multiply(num1, num2)}")
+    elif choice == '4':
+      print(f"the division result is: {divide(num1, num2)}")
   else:
     print("Invalid input")
 if __name__ =="__main__":
